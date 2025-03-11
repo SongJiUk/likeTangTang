1c1b8689f096f14ab381bd42f441a64b10d79e0b
JiUk <wldnr1014@naver.com>
2025-03-11 13:02:13 +0900

    폴더 업데이트 memo 스크립트 추가

---
+++ LikeTangTang/Assets/!Scripts/Utils/Memo.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Memo
+{
+    /*
+
+
+
+ 그리드 방식( 직접 만들기 vs Unity에서 제공하는 Grid 사용하기)
+
+
+ WorldToCell -> 그리드에 나의 월드좌표를 건내주면 셀 번호를 돌려준다.
+
+ Add
+ Remove
+ Get
+ 한방에 불러오는 코드(내 위치, Range)
+
+
+
+ ------------
+
+ 스킬
+
+ 데이터( 스킬 데이터 관리가 가장 어려움)
+ 하나에 가득 넣어버리면 관리가 힘들어진다.
+
+ 스크립트 여러개 생성 vs 하나에 넣어서 생성
+
+ 코루틴사용할때 isVaild체크 해야됌( null 체크하면 꼬일수도있음.)
+ Utils 쪽에 만들거나 Extension을 하나 만들어서 activeSelf 를 추가해서 검사해주는거임
+ 굳이 안만들어서 사용해도됨 ( activeSelf를 &&으로 검사해주면 되니까) 근데 만들어주면 편함.
+
+     */
+}
