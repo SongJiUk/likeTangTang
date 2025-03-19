f9cb055b18b3c68a85e0dd8bcd7bf3135b09c4f9
JiUk <wldnr1014@naver.com>
2025-03-19 14:12:42 +0900

    Grid사용해서 최적화하는거 다시 확인하기

--- LikeTangTang/Assets/!Scripts/Controllers/GemController.cs
+++ LikeTangTang/Assets/!Scripts/Controllers/GemController.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class GemController : BaseController
 {
 
+
+    /* TODO : Grid를 활용하여 Gem 최적화하기.
+     
+    연산을 사용할때 루트를 씌우면 부하가 심하다.기존의 값을 제곱해서 sqrMagnitude를 사용하면 됨.
+
+
+     */
     public override bool Init()
     {
         base.Init();
-
-
+        
         return true;
     }
 
