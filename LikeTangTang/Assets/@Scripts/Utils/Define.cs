cf0edd713bca719cd52eb5e211b104010af25459
JiUk <wldnr1014@naver.com>
2025-04-10 23:30:34 +0900

    스킬데이터, Define수정(내일 스킬프리팹 대충 생성해서 실험해야됌)

--- LikeTangTang/Assets/@Scripts/Utils/Define.cs
+++ LikeTangTang/Assets/@Scripts/Utils/Define.cs
@@ -30,12 +30,12 @@ public static class Define
         None,
         PlasmaShot, //일정 시간마다 전방에 플라즈마 탄환 발사 (짧은 관통 효과 있음)
         EnergyRing, // 플레이어 주변에 에너지 고리 생성, 닿으면 몬스터 데미지
-        AutoTurretDrone, // 	일정 시간동안 드론을 소환해서 자동으로 주변 공격
-        SelfDestructDrone, //일정 시간 후 폭발하는 자폭 드론을 발사
+        PlasmaBoomerang, //주변의 적에게 튕기는 부메랑을 발사합니다.
+        SuicideDrone, //일정 시간 후 폭발하는 자폭 드론을 발사
         ElectricShock, //	랜덤 주변 몬스터들에게 전기 번개 튕기면서 연쇄 데미지
         GravityBomb, // 	중력 자기장 폭탄(몬스터들을 끌어들이는 폭탄)
-        TimeSlowField, // 주변 몬스터 이동속도 50% 감소하는 필드 생성
-        OrbitalSatellites // 캐릭터 주변을 회전한다.
+        TimeStopBomb, // 주변 몬스터 이동속도 50% 감소하는 필드 생성
+        OrbitalBlade // 캐릭터 주변을 회전한다.
     }
     public enum SceneType
     {
