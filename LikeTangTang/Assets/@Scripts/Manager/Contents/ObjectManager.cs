9ca2c58700be4048f6333237a5ebdb0f94d9da48
JiUk <wldnr1014@naver.com>
2025-04-28 13:19:55 +0900

    몬스터 찾는부분 최적화

--- LikeTangTang/Assets/@Scripts/Manager/Contents/ObjectManager.cs
+++ LikeTangTang/Assets/@Scripts/Manager/Contents/ObjectManager.cs
@@ -49,6 +49,7 @@ public class ObjectManager
             Player = pc;
             return pc as T;
         }
+
         if(monsterType.IsAssignableFrom(type))
         {
             CreatureData cd = Manager.DataM.CreatureDic[_templateID];
@@ -67,52 +68,7 @@ public class ObjectManager
             return mc as T;
         }
 
-        //if (type == monsterType)
-        //{
-
-        //    //[x] DATA로 받아와서 처리하지.(스테이지 데이터를 생성하서, 그 스테이지에 맞는 몬스터들 가져오면 됌.)
-        //    CreatureData cd = Manager.DataM.CreatureDic[_templateID];
-
-        //    GameObject go = Manager.ResourceM.Instantiate(cd.prefabName, _pooling: true);
-        //    MonsterController mc = Utils.GetOrAddComponent<MonsterController>(go);
-        //    go.transform.position = _pos;
-        //    mc.Init();
-        //    mc.SetInfo(_templateID);
-        //    mc.name = cd.prefabName;
-        //    mcSet.Add(mc);
-            
-        //    return mc as T;
-
-        //}
-        //else if(type == eliteMonsterType)
-        //{
-        //    CreatureData cd = Manager.DataM.CreatureDic[_templateID];
-           
-        //    GameObject go = Manager.ResourceM.Instantiate(cd.prefabName, _pooling: true);
-        //    EliteMonsterController emc = go.GetOrAddComponent<EliteMonsterController>();
-        //    emc.transform.position = _pos;
-        //    emc.Init();
-        //    emc.SetInfo(_templateID);
-        //    emc.name = cd.prefabName;
-        //    mcSet.Add(emc);
-
-        //    return emc as T;
-        //}
-        //else if(type == bossMonsterType)
-        //{
-        //    CreatureData cd = Manager.DataM.CreatureDic[_templateID];
-
-        //    GameObject go = Manager.ResourceM.Instantiate(cd.prefabName);
-        //    BossController bc = go.GetOrAddComponent<BossController>();
-        //    go.transform.position = _pos;
-        //    bc.Init();
-        //    bc.SetInfo(_templateID);
-        //    go.name = cd.prefabName;
-        //    mcSet.Add(bc);
-
-        //    return bc as T;
-        //}
-
+       
         if(dropItemType.IsAssignableFrom(type))
         {
             GameObject go = Manager.ResourceM.Instantiate(_prefabName, null, true);
@@ -125,54 +81,13 @@ public class ObjectManager
             return dropItem;
         }
 
-        if (typeof(T).IsSubclassOf(typeof(DropItemController)))
-        {
-            GameObject go = Manager.ResourceM.Instantiate(_prefabName, null, true);
-            T obj = Utils.GetOrAddComponent<T>(go);
-            go.transform.position = _pos;
-            dropItemSet.Add(obj as DropItemController);
-
-            Grid.AddCell(go);
-
-            return obj;
-        }
-        //else if (type == gemType)
-        //{
-        //    GameObject go = Manager.ResourceM.Instantiate(Define.GEMNAME, null, true);
-        //    GemController gc = Utils.GetOrAddComponent<GemController>(go);
-        //    go.transform.position = _pos;
-        //    gemSet.Add(gc);
-
-        //    // [ ] sprite이름도 DATA에서 불러와서 효과적으로 진행
-        //    //string key = UnityEngine.Random.Range(0, 2) == 0 ? "Gem_01.sprite" : "Gem_02.sprite";
-        //    //Sprite sprite = Manager.ResourceM.Load<Sprite>(key);
-        //    //go.GetComponent<SpriteRenderer>().sprite = sprite;
-
-        //    //Explanation : AddGrid
-        //    Grid.AddCell(go);
-
-        //    return gc as T;
-
-
-        //}
-        //else if( type == dropItemType)
-        //{
-        //    GameObject go = Manager.ResourceM.Instantiate(_prefabName, null, true);
-        //    T dc = Utils.GetOrAddComponent<T>(go);
-        //    go.transform.position = _pos;
-        //    dropItemSet.Add(dc as DropItemController);
-
-        //    Grid.AddCell(go);
-
-        //    return dc as T;
-
-        //}
-        else if (type == gridType)
+        if (type == gridType)
         {
             Grid = GameObject.Find(Define.GRIDNAME).GetComponent<GridController>();
             Grid.Init();
             return Grid as T;
         }
+
         if(projectileType.IsAssignableFrom(type))
         {
             var go = Manager.ResourceM.Instantiate(_prefabName, _pooling: true);
@@ -185,19 +100,6 @@ public class ObjectManager
             return proj as T;
         }
 
-        if (type == projectileType)
-        {
-            // TemplateID 받아와서 생성
-            //if (Manager.DataM.SkillDic.TryGetValue(_templateID, out var skilldata) == false) return null;
-
-            GameObject go = Manager.ResourceM.Instantiate(_prefabName, _pooling: true);
-            ProjectileController pc = go.GetOrAddComponent<ProjectileController>();
-            go.transform.position = _pos;
-            pjSet.Add(pc);
-            pc.Init();
-
-            return pc as T;
-        }
 
         if(skillType.IsAssignableFrom(type))
         {
@@ -211,18 +113,7 @@ public class ObjectManager
 
             return skill;
         }
-        else if (type == egoSwordType)
-        {
-            if (Manager.DataM.SkillDic.TryGetValue(_templateID, out var skillData) == false) return null;
-
-            GameObject go = Manager.ResourceM.Instantiate(skillData.PrefabName, _pooling: false);
-            go.transform.position = _pos;
-
-            T t = go.GetOrAddComponent<T>();
-            t.Init();
-
-            return t;
-        }
+       
             
         return null;
     }
@@ -233,7 +124,7 @@ public class ObjectManager
         if (!_obj.IsValid()) return;
 
 
-        System.Type type = typeof(T);
+        Type type = typeof(T);
 
         if(type == playerType)
         {
@@ -245,31 +136,22 @@ public class ObjectManager
         }
         else if(monsterType.IsAssignableFrom(type))
         {
-            if(_obj.IsValid() == false) return;
-
             mcSet.Remove(_obj as MonsterController);
             Manager.ResourceM.Destory(_obj.gameObject);
         }
-        else if(type == monsterType)
+        else if(gemType.IsAssignableFrom(type))
         {
-            if(_obj.IsValid() == false) return;
-
-            mcSet.Remove(_obj as MonsterController);
-            Manager.ResourceM.Destory(_obj.gameObject);
-        }
-        else if(type == gemType)
-        {
-            if(_obj.IsValid() == false) return;
-
             gemSet.Remove(_obj as GemController);
             Manager.ResourceM.Destory(_obj.gameObject);
-
             Grid.RemoveCell(_obj.gameObject);
         }
-        //NOTE : IsAssignableFrom을 이용하면 해당 부모의 자식을 모두 찾을 수 있음.
-        else if(skillType.IsAssignableFrom(type))
+        else if(projectileType.IsAssignableFrom(type))
         {
             pjSet.Remove(_obj as ProjectileController);
+            Manager.ResourceM.Destory(_obj.gameObject);
+        }
+        else if(skillType.IsAssignableFrom(type))
+        {
             Manager.ResourceM.Destory(_obj.gameObject);
         }
     }
@@ -309,23 +191,50 @@ public class ObjectManager
         damageFont.SetInfo(_pos, _damage, _heal, _parent, _isCritical);
     }
 
-    public List<MonsterController> GetNearMonsters(int _count = 1, int distanceThreshold = 0)
+    //public List<MonsterController> GetNearMonsters(int _count = 1, int distanceThreshold = 0)
+    //{
+    //    List<MonsterController> monsterList = mcSet.OrderBy(monster => (Player.transform.position - monster.transform.position).sqrMagnitude).ToList();
+
+
+    //    if(distanceThreshold > 0)
+    //        monsterList = monsterList.Where(monster => (Player.transform.position - monster.transform.position).sqrMagnitude > distanceThreshold * distanceThreshold).ToList();
+
+    //    int min = Mathf.Min(_count, monsterList.Count);
+    //    List<MonsterController> nearsMonsters = monsterList.Take(min).ToList();
+
+    //    if(nearsMonsters.Count == 0) return null;
+
+    //    while(nearsMonsters.Count < _count)
+    //        nearsMonsters.Add(nearsMonsters.Last());
+
+    //    return nearsMonsters;
+    //}
+
+    public List<MonsterController> GetNearMonsters(int _count = 1, int _distanceThreshold = 0)
     {
-        List<MonsterController> monsterList = mcSet.OrderBy(monster => (Player.transform.position - monster.transform.position).sqrMagnitude).ToList();
-        
+        IEnumerable<MonsterController> monsters = mcSet;
 
-        if(distanceThreshold > 0)
-            monsterList = monsterList.Where(monster => (Player.transform.position - monster.transform.position).sqrMagnitude > distanceThreshold * distanceThreshold).ToList();
 
-        int min = Mathf.Min(_count, monsterList.Count);
-        List<MonsterController> nearsMonsters = monsterList.Take(min).ToList();
+        if (_distanceThreshold > 0)
+        {
+            float thresholdSqr = _distanceThreshold * _distanceThreshold;
+            monsters = monsters.Where(monster => (Player.transform.position - monster.transform.position).sqrMagnitude > thresholdSqr);
+        }
 
-        if(nearsMonsters.Count == 0) return null;
+        List<MonsterController> sortedMonsters = monsters.OrderBy(monster =>
+       (Player.transform.position - monster.transform.position).sqrMagnitude).ToList();
 
-        while(nearsMonsters.Count < _count)
+        int min = Mathf.Min(_count, sortedMonsters.Count);
+
+        List<MonsterController> nearsMonsters = sortedMonsters.Take(min).ToList();
+
+        if (nearsMonsters.Count == 0) return null;
+
+        while (nearsMonsters.Count < _count)
             nearsMonsters.Add(nearsMonsters.Last());
-         
+
         return nearsMonsters;
     }
+
 
 }
