7ff6c50ad949049a2cb86e5dd72f750998bd74ca
JiUk <wldnr1014@naver.com>
2025-05-19 13:23:01 +0900

    해야될것들 미리 세팅

---
+++ LikeTangTang/Assets/@Scripts/UI/Popup/UI_MissionPopup.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_MissionPopup : UI_Popup
+{
+    enum GameObjects
+    {
+
+    }
+
+    enum Texts
+    {
+
+    }
+
+    enum Buttons
+    {
+
+    }
+
+    enum Images
+    {
+
+    }
+
+    private void Awake()
+    {
+        Init();
+    }
+
+    public override bool Init()
+    {
+        if (!base.Init()) return false;
+        gameObjectsType = typeof(GameObjects);
+        TextsType = typeof(Texts);
+        ButtonsType = typeof(Buttons);
+        ImagesType = typeof(Images);
+
+        BindObject(gameObjectsType);
+        BindText(TextsType);
+        BindButton(ButtonsType);
+        BindImage(ImagesType);
+
+
+
+
+        return true;
+
+    }
+
+    public void SetInfo()
+    {
+
+    }
+
+    void Refresh()
+    {
+
+    }
+}
